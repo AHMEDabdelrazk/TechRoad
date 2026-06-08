82c0043bf6b7c7ad2eaf2b6806ea6457b0ce23bb
AHMEDabdelrazk <ahmedahmedahmed778899@gmail.com>
2026-06-08 12:04:41 +0300

    updateeee

--- Frontend/techroad-client/src/services/api.js
+++ Frontend/techroad-client/src/services/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// Create an Axios instance with the base URL of your backend API
 export default axios.create({
     baseURL: "https://techroad-6lc6.onrender.com/api"
 });
